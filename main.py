afb6fb38e79641313cd47d84219d54ac6c978bd4
Lucas-Fernandes-Martins <lucasfmartins16@gmail.com>
2024-04-10 15:40:14 +0200

    demo

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-from typing import Optional
-
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+from engine import get_score
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Adjust this to match your React app's URL
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Define your Python function
+def process_strings(string1: str, string2: str):
+    # Perform some operation on the input strings
+    result = f"Processed: {string1.upper()} and {string2.lower()}"
+    return result
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def parse_input(input_string):
+    return input_string.replace("backl", "\\")
+ 
+
+# Define a route to handle GET requests with two string arguments
+@app.get('/api/get_score/{string1}/{string2}')
+def get_data(string1: str, string2: str):
+    string1 = parse_input(string1)
+    string2 = parse_input(string2)
+    # Call the function with the provided input strings
+    data = get_score(string1, string2)
+    # Create JSON response
+    response = {'result': [string1, string2, data]}
+    # Return JSON response
+    return response
+
+
