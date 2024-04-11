78891b22f466b0b523ae975254c71930e4e416d1
Lucas-Fernandes-Martins <lucasfmartins16@gmail.com>
2024-04-11 19:43:27 +0200

    fix route

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust this to match your React app's URL
+    allow_origins=["https://frontend-ra.web.app"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
 
